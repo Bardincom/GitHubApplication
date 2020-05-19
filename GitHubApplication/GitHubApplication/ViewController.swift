3dd77ff4920954d3323a909258a3a2b96aaa3465
Oleksii Bardin <bardincom@icloud.com>
2020-05-20 00:03:48 +0300

    v. 0.1 Создание проекта

---
+++ GitHubApplication/GitHubApplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubApplication
+//
+//  Created by Aleksey Bardin on 19.05.2020.
+//  Copyright © 2020 Aleksey Bardin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
