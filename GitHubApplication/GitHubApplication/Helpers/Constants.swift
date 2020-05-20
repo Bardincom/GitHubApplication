a0acb1dabfafd5fbadb1aa2caf1770b43e740f30
Oleksii Bardin <bardincom@icloud.com>
2020-05-21 00:13:09 +0300

    v. 0.3 Реализовано смещение контента в зависимости от устройства.

--- GitHubApplication/GitHubApplication/Helpers/Constants.swift
+++ GitHubApplication/GitHubApplication/Helpers/Constants.swift
@@ -4,8 +4,18 @@
 //
 //  Created by Aleksey Bardin on 20.05.2020.
 //  Copyright © 2020 Aleksey Bardin. All rights reserved.
-//
+//   https://github.githubassets.com/images/modules/logos_page/GitHub-Logo.png
 
 import Foundation
+import UIKit
 
+/// загружаемое из интернета изобржение
 public let urlImage = URL(string: "https://github.githubassets.com/images/modules/logos_page/GitHub-Logo.png")
+
+public let maxHeightFrame: CGFloat = 736
+
+/// коэффициент изменяющий смещение view относительно клавиатуры, для экранов с большим и маленьким рарзешением
+enum Factor {
+  static let compact: CGFloat = 0.8
+  static let regular: CGFloat = 0.3
+}
--- GitHubApplication/GitHubApplication/LoginViewController.swift
+++ GitHubApplication/GitHubApplication/LoginViewController.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Aleksey Bardin on 19.05.2020.
 //  Copyright © 2020 Aleksey Bardin. All rights reserved.
-// https://github.githubassets.com/images/modules/logos_page/GitHub-Logo.png
+//
 
 import UIKit
 import Kingfisher
@@ -23,21 +23,91 @@ final class LoginViewController: UIViewController {
     customizeItems()
   }
 
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+
+    notificationAddObserver()
+  }
+
+  override func viewWillDisappear(_ animated: Bool) {
+    super.viewWillDisappear(animated)
+
+    notificationRemoveObserver()
+  }
+
   @IBAction func pressLoginButton(_ sender: Any) {
     print("Press Login Button")
   }
 
+  @IBAction func returnUsername(_ sender: UITextField) {
+    sender.resignFirstResponder()
+  }
+  @IBAction func returnPassword(_ sender: UITextField) {
+    sender.resignFirstResponder()
+  }
 }
 
+// MARK: Helpers Methods
 private extension LoginViewController {
+  /// установка загруженного изображения
   func setLogo() {
     let urlLogoImage = urlImage
     self.logoImageView.kf.setImage(with: urlLogoImage)
   }
 
+  /// настройка внешнего вида UI элементов
   func customizeItems() {
     usernameTextField.placeholder = "username"
     passwordTextField.placeholder = "password"
     loginButton.layer.cornerRadius = 5
+  }
+
+  /// смещает клавиатуру в зависимости от размера экрана
+  func shiftView (_ keyboardSize: CGRect,
+                  _ firstTextField: UITextField,
+                  _ secondTextField: UITextField,
+                  _ factor: CGFloat) {
+    if firstTextField.isEditing {
+      self.view.frame.origin.y = -keyboardSize.height * factor
+    } else if secondTextField.isEditing {
+      self.view.frame.origin.y = -keyboardSize.height * factor
+    }
+  }
+
+  /// сообщает что клавиатура появилась
+  func notificationAddObserver() {
+    NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+
+    NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+  }
+
+  /// сообщает что клавиатура была скрыта
+  func notificationRemoveObserver() {
+    NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+    NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+  }
+}
+
+// MARK: Selectors
+private extension LoginViewController {
+
+  @objc
+  func keyboardWillHide() {
+    self.view.frame.origin.y = 0
+  }
+
+  @objc
+  func keyboardWillChange(notification: NSNotification) {
+
+    if let size = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+
+      guard view.bounds.height > maxHeightFrame else {
+        shiftView(size, usernameTextField, passwordTextField, Factor.compact)
+        return
+      }
+
+      shiftView(size, usernameTextField, passwordTextField, Factor.regular)
+
+    }
   }
 }
