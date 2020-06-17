3797d343fa165f848a212afa3c9bfceae59ac11e
Oleksii Bardin <bardincom@icloud.com>
2020-06-17 10:46:54 +0300

    Revert "v 0.7 Создан WebViewController и логика перехода на репозиторий пользователя."
    This reverts commit 29d3b2e40b313cf9272f2e5072354ec110fcf299.

--- GitHubApplication/GitHubApplication/Networking/SessionProvider.swift
+++ GitHubApplication/GitHubApplication/Networking/SessionProvider.swift
@@ -17,15 +17,69 @@ final class SessionProvider: NSObject {
   private let hostPath = "https://api.github.com"
   private let searchRepoPath = "/search/repositories"
 
+  /// Поиск по GITHub
+  /// - Parameters:
+  ///   - name: имя репозитория
+  ///   - language: язык репозитория
+  ///   - order: выбор лучшего совпадения  desc или asc
+//  func searchRepositiries(name: String?, language: String?, order: String, completionHandler: @escaping ([Repository]) -> Void) {
+//    var urlComponents = URLComponents()
+//    urlComponents.scheme = scheme
+//    urlComponents.host = host
+//    urlComponents.path = searchRepoPath
+//
+//    guard let name = name, let language = language else { return }
+//
+//    switch language {
+//      case "":
+//        urlComponents.queryItems = [
+//          URLQueryItem(name: "q", value: "\(name)"),
+//          URLQueryItem(name: "sort", value: "star"),
+//          URLQueryItem(name: "order", value: order)
+//        ]
+//      default:
+//        urlComponents.queryItems = [
+//          URLQueryItem(name: "q", value: "\(name)+language:\(language)"),
+//          URLQueryItem(name: "sort", value: "stars"),
+//          URLQueryItem(name: "order", value: order)
+//        ]
+//    }
+//
+//    guard let url = urlComponents.url else { return }
+////    print(url)
+//
+//    let dataTask = sharedSession.dataTask(with: url) { (data, response, error) in
+//      if let error = error {
+//        print(error.localizedDescription)
+//        return
+//      }
+//
+//      guard let resquestData = data else { return }
+//      DispatchQueue.global(qos: .userInitiated).async {
+//        do {
+//          let search = try self.decoder.decode(Search.self, from: resquestData)
+//          guard let repositories = search.repositories else { return }
+//
+//          DispatchQueue.main.async {
+//            completionHandler(repositories)
+//          }
+//
+//        } catch let error {
+//          print("Error", error)
+//        }
+//      }
+//
+//    }
+//
+//    dataTask.resume()
+//  }
+
   /// Подготовка URL строки для дальнейшего поиска
   /// - Parameters:
   ///   - name: имя репозитория
   ///   - language: язык репозитория
   ///   - order: выбор лучшего совпадения  desc или asc
-  private func preparationURL(name: String?,
-                              language: String?,
-                              order: String) -> URL? {
-
+  private func preparationURL(name: String?, language: String?, order: String) -> URL? {
     var urlComponents = URLComponents()
     urlComponents.scheme = scheme
     urlComponents.host = host
